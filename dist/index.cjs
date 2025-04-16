4a3d057fe892dd50dd28231c76dd8c9cb1ba3b55
sabarivasan216 <sabariv669@gmail.com>
2025-04-16 16:34:14 +0530

    first commit

---
+++ dist/index.cjs
@@ -0,0 +1,19 @@
+'use strict';
+
+async function getData() {
+  const url = "https://jsonplaceholder.typicode.com/users";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const json = await response.json();
+    console.log(json);
+    return json;
+  } catch (error) {
+    console.error(error.message);
+    return [];
+  }
+}
+
+exports.getData = getData;
